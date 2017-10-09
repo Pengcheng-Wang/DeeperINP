ffc2696d1c6e21d2cf40f2b9c7a39d77bc8d4c95
Pengcheng Wang <pwang8@ncsu.edu>
2017-10-08 18:14:32 -0600

    testing

--- UserSimLearner/CIUserSimEnv.lua
+++ UserSimLearner/CIUserSimEnv.lua
@@ -128,7 +128,7 @@ function CIUserSimEnv:_calcUserAct()
         -- bipartite action, outcome (score) prediction models
         if self.opt.uppModel == 'lstm' then
             self.userActsPred:forget()
-            print(self.userActsPred:forward(self.tabRnnStatePrep))
+            print(self.userActsPred)
             os.exit()
             nll_acts = self.userActsPred:forward(self.tabRnnStatePrep)[self.opt.lstmHist]:squeeze() -- get act choice output for last time step
         else
