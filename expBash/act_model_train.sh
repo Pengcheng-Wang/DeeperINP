27c1a4a65d8d1b91be0d3c83e7a55a29e62bc644
Pengcheng Wang <pwang8@ncsu.edu>
2017-11-02 18:29:21 -0400

    Modified training iteration number to 800

--- expBash/act_model_train.sh
+++ expBash/act_model_train.sh
@@ -30,7 +30,7 @@
 #done
 
 ### The following experiment compare the effects of data augmentation without batch normalization but with dropout
-for s in `seq 1 3`;
+for s in `seq 1 2`;
 do
     ## Train models without data augmentation
     for t in `seq 1 5`;
@@ -40,7 +40,7 @@ do
         then
             g=1
         fi
-        th userSimMain.lua -trType ac -save rnndrop/seed$(($s))/no_aug/tdiv$(($t))/ -batchSize 80 -coefL2 5e-3 -lstmHd 64 -ciuTType train -uppModel lstm -lstmHist 10 -usimTrIte 400  -uSimShLayer 0 -testSetDivSeed $(($t-1)) -gpu $(($g)) -dropoutUSim 0.25 -actPredDataAug 0 -seed $(($s)) &
+        th userSimMain.lua -trType ac -save rnndrop/seed$(($s))/no_aug/tdiv$(($t))/ -batchSize 80 -coefL2 5e-3 -lstmHd 64 -ciuTType train -uppModel lstm -lstmHist 10 -usimTrIte 800  -uSimShLayer 0 -testSetDivSeed $(($t-1)) -gpu $(($g)) -dropoutUSim 0.25 -actPredDataAug 0 -seed $(($s)) &
     done
     wait
     ## Train models with data augmentation
@@ -51,7 +51,7 @@ do
         then
             g=1
         fi
-        th userSimMain.lua -trType ac -save rnndrop/seed$(($s))/aug/tdiv$(($t))/ -batchSize 160 -coefL2 5e-3 -lstmHd 64 -ciuTType train -uppModel lstm -lstmHist 10 -usimTrIte 400  -uSimShLayer 0 -testSetDivSeed $(($t-1)) -gpu $(($g)) -dropoutUSim 0.25 -actPredDataAug 1 -seed $(($s)) &
+        th userSimMain.lua -trType ac -save rnndrop/seed$(($s))/aug/tdiv$(($t))/ -batchSize 160 -coefL2 5e-3 -lstmHd 64 -ciuTType train -uppModel lstm -lstmHist 10 -usimTrIte 800  -uSimShLayer 0 -testSetDivSeed $(($t-1)) -gpu $(($g)) -dropoutUSim 0.25 -actPredDataAug 1 -seed $(($s)) &
     done
     wait
 done
