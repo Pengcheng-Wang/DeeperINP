c2246c78fe0b4fed42fb1d90bb11ad85e085c5e5
Pengcheng Wang <pwang8@ncsu.edu>
2017-11-02 23:07:12 -0400

    test speed

--- expBash/act_model_train.sh
+++ expBash/act_model_train.sh
@@ -42,9 +42,9 @@ do
         fi
         if [ $t -eq 1 ];
         then
-            th userSimMain.lua -trType ac -save rnndrop.1/seed$(($s))/no_aug/tdiv$(($t))/ -batchSize 80 -coefL2 5e-3 -lstmHd 64 -ciuTType train -uppModel lstm -lstmHist 10 -usimTrIte 400  -uSimShLayer 0 -testSetDivSeed $(($t-1)) -gpu $(($g)) -dropoutUSim 0.1 -actPredDataAug 0 -seed $(($s)) &
+            th userSimMain.lua -trType ac -save rnndrop.1/seed$(($s))/no_aug/tdiv$(($t))/ -batchSize 80 -coefL2 5e-3 -lstmHd 64 -ciuTType train -uppModel lstm -lstmHist 10 -usimTrIte 400  -uSimShLayer 0 -testSetDivSeed $(($t-1)) -gpu $(($g)) -dropoutUSim 0 -actPredDataAug 0 -seed $(($s)) &
         else
-            th userSimMain.lua -trType ac -save rnndrop.1/seed$(($s))/no_aug/tdiv$(($t))/ -batchSize 80 -coefL2 5e-3 -lstmHd 64 -ciuTType train -uppModel lstm -lstmHist 10 -usimTrIte 400  -uSimShLayer 0 -testSetDivSeed $(($t-1)) -gpu $(($g)) -dropoutUSim 0.1 -actPredDataAug 0 -seed $(($s)) > /dev/null &
+            th userSimMain.lua -trType ac -save rnndrop.1/seed$(($s))/no_aug/tdiv$(($t))/ -batchSize 80 -coefL2 5e-3 -lstmHd 64 -ciuTType train -uppModel lstm -lstmHist 10 -usimTrIte 400  -uSimShLayer 0 -testSetDivSeed $(($t-1)) -gpu $(($g)) -dropoutUSim 0 -actPredDataAug 0 -seed $(($s)) > /dev/null &
         fi
     done
     wait
