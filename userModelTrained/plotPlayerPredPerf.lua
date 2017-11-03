75b6de8bc42b2374254c563c9b9425238e44fea4
Pengcheng Wang <pwang8@deeplearn.csc.ncsu.edu>
2017-11-03 16:24:03 -0400

    Added test-set only plot graph

--- userModelTrained/plotPlayerPredPerf.lua
+++ userModelTrained/plotPlayerPredPerf.lua
@@ -16,7 +16,7 @@ for _randSeed=1, 10 do
         for _line in _accFile:lines('*l') do
             local _oneLine = _line:split('\t')
             if #_oneLine == 3 then
-                _actAccNoAug[#_actAccNoAug + 1] = tonumber(string.sub(_oneLine[2], 1, -2)) -- the 2nd item in each line is the act prediction accuracy, and need to take out the % char at the end
+                _actAccNoAug[#_actAccNoAug + 1] = tonumber(string.sub(_oneLine[2], 1, -2))/100.0 -- the 2nd item in each line is the act prediction accuracy, and need to take out the % char at the end
             end
         end
         -- read training-set data from file
@@ -24,10 +24,7 @@ for _randSeed=1, 10 do
         local _actAccNoAugTrain = {}
         local _ = _accFile:read() -- This is the header in the file, read and ignore this line
         for _line in _accFile:lines('*l') do
-            local _oneLine = _line:split('\t')
-            if #_oneLine == 3 then
-                _actAccNoAugTrain[#_actAccNoAugTrain + 1] = tonumber(string.sub(_oneLine[2], 1, -2)) -- the 2nd item in each line is the act prediction accuracy, and need to take out the % char at the end
-            end
+            _actAccNoAugTrain[#_actAccNoAugTrain + 1] = tonumber(_line)/100.0 -- only one item in train.log in each row
         end
         -- now read the test-set file from the aug directory
         _accFile = io.open(paths.concat(_resDic, string.format('aug/tdiv%d', _testSeed), 'test.log'), 'r')
@@ -36,7 +33,7 @@ for _randSeed=1, 10 do
         for _line in _accFile:lines('*l') do
             local _oneLine = _line:split('\t')
             if #_oneLine == 3 then
-                _actAccAug[#_actAccAug + 1] = tonumber(string.sub(_oneLine[2], 1, -2)) -- the 2nd item in each line is the act prediction accuracy, and need to take out the % char at the end
+                _actAccAug[#_actAccAug + 1] = tonumber(string.sub(_oneLine[2], 1, -2))/100.0 -- the 2nd item in each line is the act prediction accuracy, and need to take out the % char at the end
             end
         end
         -- now read the training-set file from the aug directory
@@ -44,16 +41,24 @@ for _randSeed=1, 10 do
         local _actAccAugTrain = {}
         local _ = _accFile:read()   -- Pass the first row/line in the file, which contains header
         for _line in _accFile:lines('*l') do
-            local _oneLine = _line:split('\t')
-            if #_oneLine == 3 then
-                _actAccAugTrain[#_actAccAugTrain + 1] = tonumber(string.sub(_oneLine[2], 1, -2)) -- the 2nd item in each line is the act prediction accuracy, and need to take out the % char at the end
-            end
+            _actAccAugTrain[#_actAccAugTrain + 1] = tonumber(_line)/100.0 -- only one acc in train.log
         end
         -- Can draw the figure using the two tables, need to construct tensors based on the two tables
         gnuplot.plot({'no aug test', torch.Tensor(_actAccNoAug), '-'}, {'aug test', torch.Tensor(_actAccAug), '-'}, {'no aug train', torch.Tensor(_actAccNoAugTrain), '-'}, {'aug train', torch.Tensor(_actAccAugTrain), '-'})
         gnuplot.raw('set xtics <INCREMENT>')
         gnuplot.raw('set term png size 1000, 400')
         gnuplot.raw('set yr [GPVAL_DATA_Y_MIN:GPVAL_DATA_Y_MAX]')
+        --gnuplot.raw('set size 1.5,2')
+        gnuplot.plotflush()
+        gnuplot.axis({1, math.max(#_actAccNoAug, #_actAccAug), 0, ''})
+        gnuplot.title(string.format('Act Pred Accu when testSeed is %d', _testSeed))
+
+        gnuplot.pngfigure(paths.concat(_resDic, string.format('actPA%dTestOnly.png', _testSeed)))
+        gnuplot.plot({'no aug test', torch.Tensor(_actAccNoAug), '-'}, {'aug test', torch.Tensor(_actAccAug), '-'})
+        gnuplot.raw('set xtics <INCREMENT>')
+        gnuplot.raw('set term png size 1000, 400')
+        -- gnuplot.raw('set yr [GPVAL_DATA_Y_MIN:GPVAL_DATA_Y_MAX]')
+        gnuplot.raw('set yrange [0.2:0.45]')
         --gnuplot.raw('set size 1.5,2')
         gnuplot.plotflush()
         gnuplot.axis({1, math.max(#_actAccNoAug, #_actAccAug), 0, ''})
