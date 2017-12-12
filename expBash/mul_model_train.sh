20ed0a439642b92ae8e03e06b2302c2fd3760387
Pengcheng Wang <pwang8@ncsu.edu>
2017-12-12 10:27:57 -0500

    Added exp

--- expBash/mul_model_train.sh
+++ expBash/mul_model_train.sh
@@ -67,31 +67,61 @@
 #    date +%Y,%m,%d-%H:%M:%S
 #done
 
-# Dec 11, 2017.
-# Try to do score/outcome prediction using rhn models, without using multi-task structure. This time with shorter valid score prediction sequence
+## Dec 11, 2017.
+## Try to do score/outcome prediction using rhn models, without using multi-task structure. This time with shorter valid score prediction sequence
+#s=1
+#for rnnHdLc in 1 2 3
+#do
+#    for recD in 5
+#    do
+#        echo 'For outcome prediction, in B-LSTM round' ${rnnHdLc}
+#        date +%Y,%m,%d-%H:%M:%S
+#        for t in `seq 1 2`;
+#        do
+#            g=0
+#            if [ $t -eq 2 ]
+#            then
+#                g=0
+#            fi
+#            if [ $t -eq 1 ];
+#            then
+#                th userSimMain.lua -trType sc -save sc_blstm_hdlc_${rnnHdLc}_sh/rnndrop.1/seed$(($s))/augRnd/tdiv$(($t))/ -batchSize 160 -coefL2 1e-2 -rnnHdSizeL1 21 -rnnHdLyCnt $(($rnnHdLc)) -rhnReccDept $(($recD)) -ciuTType train -uppModel rnn_blstm -lstmHist 10 -usimTrIte 5000  -uSimShLayer 0 -testSetDivSeed $(($t-1)) -gpu $(($g)) -dropoutUSim 0.1 -learningRate 1e-4 -uSimLstmBackLen 3 -actPredDataAug 1 -seed $(($s)) -scorePredStateScope 15 &
+#            else
+#                th userSimMain.lua -trType sc -save sc_blstm_hdlc_${rnnHdLc}_sh/rnndrop.1/seed$(($s))/augRnd/tdiv$(($t))/ -batchSize 160 -coefL2 1e-2 -rnnHdSizeL1 21 -rnnHdLyCnt $(($rnnHdLc)) -rhnReccDept $(($recD)) -ciuTType train -uppModel rnn_blstm -lstmHist 10 -usimTrIte 5000  -uSimShLayer 0 -testSetDivSeed $(($t-1)) -gpu $(($g)) -dropoutUSim 0.1 -learningRate 1e-4 -uSimLstmBackLen 3 -actPredDataAug 1 -seed $(($s)) -scorePredStateScope 15 > /dev/null &
+#            fi
+#        done
+#        wait
+#        echo 'done with 2 sets in B-LSTM'  ${rnnHdLc}
+#        date +%Y,%m,%d-%H:%M:%S
+#    done
+#done
+
+
+# Dec 12, 2017.
+# Try to do score/outcome prediction using cnn moe models
 s=1
-for rnnHdLc in 1 2 3
+for moeExpCnt in 16 24 32
 do
     for recD in 5
     do
-        echo 'For outcome prediction, in B-LSTM round' ${rnnHdLc}
+        echo 'For outcome prediction, in CNN-moe round' ${moeExpCnt}
         date +%Y,%m,%d-%H:%M:%S
-        for t in `seq 1 2`;
+        for t in 1 2 3 4 5
         do
             g=0
             if [ $t -eq 2 ]
             then
-                g=0
+                g=1
             fi
             if [ $t -eq 1 ];
             then
-                th userSimMain.lua -trType sc -save sc_blstm_hdlc_${rnnHdLc}_sh/rnndrop.1/seed$(($s))/augRnd/tdiv$(($t))/ -batchSize 160 -coefL2 1e-2 -rnnHdSizeL1 21 -rnnHdLyCnt $(($rnnHdLc)) -rhnReccDept $(($recD)) -ciuTType train -uppModel rnn_blstm -lstmHist 10 -usimTrIte 5000  -uSimShLayer 0 -testSetDivSeed $(($t-1)) -gpu $(($g)) -dropoutUSim 0.1 -learningRate 1e-4 -uSimLstmBackLen 3 -actPredDataAug 1 -seed $(($s)) -scorePredStateScope 15 &
+                th userSimMain.lua -trType sc -save sc_cnnmoe_L2K1_exp_${moeExpCnt}_sh/rnndrop.1/seed$(($s))/augRnd/tdiv$(($t))/ -batchSize 160 -coefL2 1e-2 -rnnHdSizeL1 21 -rnnHdLyCnt 2 -moeExpCnt $(($moeExpCnt)) -ciuTType train -uppModel cnn_uSimCnn_moe -uSimScSoft 1 -lstmHist 3 -cnnKernelWidth 1 -usimTrIte 1500  -uSimShLayer 0 -testSetDivSeed $(($t-1)) -gpu $(($g)) -dropoutUSim 0.1 -learningRate 5e-5 -uSimLstmBackLen 3 -actPredDataAug 1 -seed $(($s)) -cnnConnType v4 -scorePredStateScope 30 &
             else
-                th userSimMain.lua -trType sc -save sc_blstm_hdlc_${rnnHdLc}_sh/rnndrop.1/seed$(($s))/augRnd/tdiv$(($t))/ -batchSize 160 -coefL2 1e-2 -rnnHdSizeL1 21 -rnnHdLyCnt $(($rnnHdLc)) -rhnReccDept $(($recD)) -ciuTType train -uppModel rnn_blstm -lstmHist 10 -usimTrIte 5000  -uSimShLayer 0 -testSetDivSeed $(($t-1)) -gpu $(($g)) -dropoutUSim 0.1 -learningRate 1e-4 -uSimLstmBackLen 3 -actPredDataAug 1 -seed $(($s)) -scorePredStateScope 15 > /dev/null &
+                th userSimMain.lua -trType sc -save sc_cnnmoe_L2K1_exp_${moeExpCnt}_sh/rnndrop.1/seed$(($s))/augRnd/tdiv$(($t))/ -batchSize 160 -coefL2 1e-2 -rnnHdSizeL1 21 -rnnHdLyCnt 2 -moeExpCnt $(($moeExpCnt)) -ciuTType train -uppModel cnn_uSimCnn_moe -uSimScSoft 1 -lstmHist 3 -cnnKernelWidth 1 -usimTrIte 1500  -uSimShLayer 0 -testSetDivSeed $(($t-1)) -gpu $(($g)) -dropoutUSim 0.1 -learningRate 5e-5 -uSimLstmBackLen 3 -actPredDataAug 1 -seed $(($s)) -cnnConnType v4 -scorePredStateScope 30 > /dev/null &
             fi
         done
         wait
-        echo 'done with 2 sets in B-LSTM'  ${rnnHdLc}
+        echo 'done with 2 sets in CNN-moe L2'  ${moeExpCnt}
         date +%Y,%m,%d-%H:%M:%S
     done
 done
