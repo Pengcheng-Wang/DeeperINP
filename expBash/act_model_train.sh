14e8b41f25585e7c003d4e991b78b6f149801d7a
Pengcheng Wang <pwang8@ncsu.edu>
2017-12-24 11:01:10 -0500

    Added exp for rhn action pred

--- expBash/act_model_train.sh
+++ expBash/act_model_train.sh
@@ -877,16 +877,47 @@
 #done
 
 
+### Testing hyper-params for action prediction models
+#echo 'in Bayesian RHN'
+#date +%Y,%m,%d-%H:%M:%S
+#for rnnHdLc in 3 4;
+#do
+#    for rhnRD in 5 8;
+#    do
+#        for t in 1 2;
+#        do
+#            for alr in 5e-4;
+#            do
+#                g=0
+#                if [ $t -eq 2 ]
+#                then
+#                    g=0
+#                fi
+#                if [ $t -eq 1 ];
+#                then
+#                    th userSimMain.lua -trType ac -save ac_rhn_alr_${alr}_hdlc_${rnnHdLc}_rhnRD_${rhnRD}/rnndrop.1/seed$(($s))/augRnd/tdiv$(($t))/ -batchSize 160 -coefL2 5e-3 -learningRate ${alr} -rnnHdSizeL1 21 -rnnHdLyCnt $(($rnnHdLc)) -rhnReccDept ${rhnRD} -ciuTType train -uppModel rnn_rhn -lstmHist 10 -usimTrIte 7000  -uSimShLayer 0 -testSetDivSeed $(($t-1)) -gpu $(($g)) -dropoutUSim 0.1 -uSimLstmBackLen 3 -actPredDataAug 1 -seed $(($s)) > /dev/null &
+#                else
+#                    th userSimMain.lua -trType ac -save ac_rhn_alr_${alr}_hdlc_${rnnHdLc}_rhnRD_${rhnRD}/rnndrop.1/seed$(($s))/augRnd/tdiv$(($t))/ -batchSize 160 -coefL2 5e-3 -learningRate ${alr} -rnnHdSizeL1 21 -rnnHdLyCnt $(($rnnHdLc)) -rhnReccDept ${rhnRD} -ciuTType train -uppModel rnn_rhn -lstmHist 10 -usimTrIte 7000  -uSimShLayer 0 -testSetDivSeed $(($t-1)) -gpu $(($g)) -dropoutUSim 0.1 -uSimLstmBackLen 3 -actPredDataAug 1 -seed $(($s)) > /dev/null &
+#                fi
+#            done
+#        done
+#    done
+#done
+#wait
+#echo 'Done with in Bayesian RHN'
+#date +%Y,%m,%d-%H:%M:%S
+
+
 ## Testing hyper-params for action prediction models
 echo 'in Bayesian RHN'
 date +%Y,%m,%d-%H:%M:%S
 for rnnHdLc in 3 4;
 do
-    for rhnRD in 5 8;
+    for rhnRD in 3 5 8;
     do
         for t in 1 2;
         do
-            for alr in 5e-4;
+            for alr in 2e-3;
             do
                 g=0
                 if [ $t -eq 2 ]
