5da3b5df58f831de6735cd256d0dbba0448c223a
Pengcheng Wang <pwang8@ncsu.edu>
2017-12-24 19:23:06 -0500

    Added exp

--- expBash/act_model_train.sh
+++ expBash/act_model_train.sh
@@ -941,15 +941,16 @@
 
 
 ## Testing hyper-params for action prediction models. Dec 24, 2017
+## Residual RHN is utilized here.
 echo 'in Bayesian RHN'
 date +%Y,%m,%d-%H:%M:%S
 for rnnHdLc in 3 4;
 do
-    for rhnRD in 2 3 5;
+    for rhnRD in 3 5;
     do
         for t in 1 2;
         do
-            for alr in 2e-3;
+            for alr in 2e-3 5e-3;
             do
                 g=0
                 if [ $t -eq 2 ]
@@ -958,9 +959,9 @@ do
                 fi
                 if [ $t -eq 1 ];
                 then
-                    th userSimMain.lua -trType ac -save ac_rhn_alr_${alr}_hdlc_${rnnHdLc}_rhnRD_${rhnRD}/rnndrop.1/seed$(($s))/augRnd/tdiv$(($t))/ -batchSize 160 -coefL2 5e-3 -learningRate ${alr} -rnnHdSizeL1 21 -rnnHdLyCnt $(($rnnHdLc)) -rhnReccDept ${rhnRD} -ciuTType train -uppModel rnn_rhn -lstmHist 10 -usimTrIte 7000  -uSimShLayer 0 -testSetDivSeed $(($t-1)) -gpu $(($g)) -dropoutUSim 0.1 -uSimLstmBackLen 3 -actPredDataAug 1 -seed $(($s)) > /dev/null &
+                    th userSimMain.lua -trType ac -save ac_rhn_res_alr_${alr}_hdlc_${rnnHdLc}_rhnRD_${rhnRD}/rnndrop.1/seed$(($s))/augRnd/tdiv$(($t))/ -batchSize 160 -coefL2 5e-3 -learningRate ${alr} -rnnHdSizeL1 21 -rnnHdLyCnt $(($rnnHdLc)) -rhnReccDept ${rhnRD} -rnnResidual 1 -ciuTType train -uppModel rnn_rhn -lstmHist 10 -usimTrIte 7000  -uSimShLayer 0 -testSetDivSeed $(($t-1)) -gpu $(($g)) -dropoutUSim 0.1 -uSimLstmBackLen 3 -actPredDataAug 1 -seed $(($s)) > /dev/null &
                 else
-                    th userSimMain.lua -trType ac -save ac_rhn_alr_${alr}_hdlc_${rnnHdLc}_rhnRD_${rhnRD}/rnndrop.1/seed$(($s))/augRnd/tdiv$(($t))/ -batchSize 160 -coefL2 5e-3 -learningRate ${alr} -rnnHdSizeL1 21 -rnnHdLyCnt $(($rnnHdLc)) -rhnReccDept ${rhnRD} -ciuTType train -uppModel rnn_rhn -lstmHist 10 -usimTrIte 7000  -uSimShLayer 0 -testSetDivSeed $(($t-1)) -gpu $(($g)) -dropoutUSim 0.1 -uSimLstmBackLen 3 -actPredDataAug 1 -seed $(($s)) > /dev/null &
+                    th userSimMain.lua -trType ac -save ac_rhn_res_alr_${alr}_hdlc_${rnnHdLc}_rhnRD_${rhnRD}/rnndrop.1/seed$(($s))/augRnd/tdiv$(($t))/ -batchSize 160 -coefL2 5e-3 -learningRate ${alr} -rnnHdSizeL1 21 -rnnHdLyCnt $(($rnnHdLc)) -rhnReccDept ${rhnRD} -rnnResidual 1 -ciuTType train -uppModel rnn_rhn -lstmHist 10 -usimTrIte 7000  -uSimShLayer 0 -testSetDivSeed $(($t-1)) -gpu $(($g)) -dropoutUSim 0.1 -uSimLstmBackLen 3 -actPredDataAug 1 -seed $(($s)) > /dev/null &
                 fi
             done
         done
