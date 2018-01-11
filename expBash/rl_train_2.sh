e7dc307bad07b802e3cc1e8bdfd253fe553806a3
Pengcheng Wang <pwang8@deeplearn.csc.ncsu.edu>
2018-01-10 22:48:53 -0500

    Moves to local machine for editing

--- expBash/rl_train_2.sh
+++ expBash/rl_train_2.sh
@@ -21,6 +21,49 @@
 #date +%Y,%m,%d-%H:%M:%S
 
 
+# ## Train DQN RL models using Ep50 usp with sampling temperature of 0.33 0.2 0.45
+# echo 'DQN RL training test'
+# date +%Y,%m,%d-%H:%M:%S
+# hist=3
+# for thrN in 6;
+# do
+#     for lrt in 7e-4;
+#     do
+#         for sfxTem in 0.33 0.2 0.45;
+#         do
+#             th rlMain.lua -async OneStepQ -ppo_clip_thr 0.15 -eta ${lrt} -momentum 0.99 -bootstraps 0 -batchSize 32 -hiddenSize 64 -doubleQ false -duel false -recurrent false -optimiser adam -threads ${thrN} -steps $((thrN*1000000)) -tau 4 -memSize 18000 -epsilonSteps 1000000 -valFreq 6000 -valSteps 5000 -bootstraps 0 -PALpha 0 -entropyBeta 0.001 -lstmHist 10 -rnnHdSizeL1 21 -rnnHdLyCnt 3 -lstmHistUsp 2 -rnnHdSizeL1Usp 21 -rnnHdLyCntUsp 2 -learnStart 2000 -progFreq 1000 -histLen ${hist} -asyncOptimFreq 1 -ciunet rlLoad -uSimScSoft 1 -uppModel rnn_rhn -uppModelUsp cnn_moe -uapFile uap_rhnL3RD3.t7 -uspFile usp_cnnmoe_L2H2K1E30e23Ep50.t7 -_id ci_dqn_lrt_${lrt}_thr_${thrN}_hist${hist}_rwd0_sfxTem${sfxTem} -async_valErr_coef 1 -ppo_optim_epo 2 -ciGroup2rwd 0 -ciRwdStMxTemp ${sfxTem} > /dev/null &
+#             wait
+#             echo 'done with thrN: ' ${thrN} ', lrt: ' ${lrt} ', sfxTem: ' ${sfxTem}
+#         done
+#     done
+# done
+# wait
+# echo 'Done training'
+# date +%Y,%m,%d-%H:%M:%S
+
+
+# ## Evaluation
+# ## Train PPO RL models using Ep50 usp with sampling temperature of 0.33 0.2 0.45. Evaluation
+# echo 'PPO RL training test'
+# date +%Y,%m,%d-%H:%M:%S
+# hist=3
+# for thrN in 6;
+# do
+#     for lrt in 7e-4;
+#     do
+#         for sfxTem in 0.33;
+#         do
+#             th rlMain.lua -async PPO -ppo_clip_thr 0.15 -mode eval -evaTrajs 10000 -ciuTType train -network experiments/ci_ppo_lrt_${lrt}_thr_${thrN}_hist${hist}_rwd0_sfxTem${sfxTem}_ppoEp2/best.weights.t7 -eta ${lrt} -momentum 0.99 -bootstraps 0 -batchSize 32 -hiddenSize 64 -doubleQ false -duel false -recurrent false -optimiser adam -threads ${thrN} -steps $((thrN*1000000)) -tau 4 -memSize 18000 -epsilonSteps 1000000 -valFreq 6000 -valSteps 5000 -bootstraps 0 -PALpha 0 -entropyBeta 0.001 -lstmHist 10 -rnnHdSizeL1 21 -rnnHdLyCnt 3 -lstmHistUsp 2 -rnnHdSizeL1Usp 21 -rnnHdLyCntUsp 2 -learnStart 2000 -progFreq 1000 -histLen ${hist} -asyncOptimFreq 1 -ciunet rlLoad -uSimScSoft 1 -uppModel rnn_rhn -uppModelUsp cnn_moe -uapFile uap_rhnL3RD3.t7 -uspFile usp_cnnmoe_L2H2K1E30e23Ep50.t7 -_id ci_eval_tem1 -async_valErr_coef 1 -ppo_optim_epo 2 -ciGroup2rwd 0 -ciRwdStMxTemp ${sfxTem}
+#             wait
+#             echo 'done with thrN: ' ${thrN} ', lrt: ' ${lrt} ', sfxTem: ' ${sfxTem}
+#         done
+#     done
+# done
+# wait
+# echo 'Done training'
+# date +%Y,%m,%d-%H:%M:%S
+
+
 ## Train DQN RL models using Ep50 usp with sampling temperature of 0.33 0.2 0.45
 echo 'DQN RL training test'
 date +%Y,%m,%d-%H:%M:%S
