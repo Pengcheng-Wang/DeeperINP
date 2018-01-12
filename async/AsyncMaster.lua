2b1c89f29ca1c5cd2e45d15c87ac9875903da32a
Pengcheng Wang <pwang8@ncsu.edu>
2018-01-12 16:39:21 -0500

    Added checking code for program finishing training

--- async/AsyncMaster.lua
+++ async/AsyncMaster.lua
@@ -172,6 +172,7 @@ function AsyncMaster:start()
   if self.opt.network ~= '' and paths.filep(self.stateFile) then
       local state = torch.load(self.stateFile)
       stepsToGo = math.floor((self.opt.steps - state.globalSteps) / self.opt.threads)
+      if stepsToGo <= 0 then exit() end
       startStep = math.floor(state.globalSteps / self.opt.threads)
       self.atomic:set(state.globalSteps)
       log.info('Resuming training from step %d', state.globalSteps)
