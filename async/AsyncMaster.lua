336733866f09cb43a7ba95f08d4d85bb90b0eaa2
Pengcheng Wang <pwang8@ncsu.edu>
2018-01-12 17:06:58 -0500

    Fixed a typo

--- async/AsyncMaster.lua
+++ async/AsyncMaster.lua
@@ -173,7 +173,7 @@ function AsyncMaster:start()
       local state = torch.load(self.stateFile)
       stepsToGo = math.floor((self.opt.steps - state.globalSteps) / self.opt.threads)
       startStep = math.floor(state.globalSteps / self.opt.threads)
-      if stepsToGo <= 0 or startStep < 0 then exit() end
+      if stepsToGo <= 0 or startStep < 0 then os.exit() end
       self.atomic:set(state.globalSteps)
       log.info('Resuming training from step %d', state.globalSteps)
       log.info('Loading pretrained network weights')
