8b16148e3fa0de6a1dff67f64d75c16d994f7b2c
Pengcheng Wang <pwang8@ncsu.edu>
2018-01-13 12:46:55 -0500

    change ppo clip thre to 0.05 and exp

--- expBash/1f_rl_train_1.sh
+++ expBash/1f_rl_train_1.sh
@@ -1,6 +1,33 @@
 #!/usr/bin/env bash
 
+### Continue to train PPO RL models using usp with sampling temperature of -1, 0.67, 0.33
+#echo 'PPO RL training with action temperature'
+#date +%Y,%m,%d-%H:%M:%S
+#hist=3
+#for actSfxTem in 0.8 0.6 0.5;
+#do
+#    for sfxTem in -1 0.5;
+#    do
+#        for seedSet in 2;
+#        do
+#            for thrN in 4;
+#            do
+#                for lrt in 7e-4;
+#                do
+#                    th rlMain.lua -async PPO -network last -seed ${seedSet} -ppo_clip_thr 0.1 -eta ${lrt} -momentum 0.99 -bootstraps 0 -batchSize 32 -hiddenSize 32 -doubleQ false -duel false -recurrent false -optimiser adam -threads ${thrN} -steps $((thrN*120000)) -tau 4 -memSize 18000 -epsilonSteps 90000 -valFreq 1500 -valSteps 2100 -bootstraps 0 -PALpha 0 -entropyBeta 0.001 -lstmHist 10 -rnnHdSizeL1 21 -rnnHdLyCnt 3 -lstmHistUsp 2 -rnnHdSizeL1Usp 21 -rnnHdLyCntUsp 2 -learnStart 2000 -progFreq 1000 -histLen ${hist} -asyncOptimFreq 1 -ciunet rlLoad -uSimScSoft 1 -uppModel rnn_rhn -uppModelUsp cnn_moe -uapFile uap_rhnL3RD3.t7 -uspFile usp_cnnmoe_L2H2K1E30e23.t7 -_id 1foldCI/ci_ppo_lrt_${lrt}_thr_${thrN}_hist${hist}_rwd0_RwdTem${sfxTem}_ActTem${actSfxTem}_ppoEp3_seed${seedSet} -async_valErr_coef 1 -ppo_optim_epo 3 -ciGroup2rwd 0 -ciRwdStMxTemp ${sfxTem} -ciActStMxTemp ${actSfxTem} > /dev/null &
+#                    wait
+#                    echo 'done with thrN: ' ${thrN} ', lrt: ' ${lrt} ', RwdsfxTem: ' ${sfxTem} ', ActsfxTem: ' ${actSfxTem}
+#                done
+#            done
+#        done
+#    done
+#done
+#echo 'Done training'
+#date +%Y,%m,%d-%H:%M:%S
+
+
 ## Continue to train PPO RL models using usp with sampling temperature of -1, 0.67, 0.33
+## when using full data set based user simulator, using ppo optimization epoch of 3 is not a good choice
 echo 'PPO RL training with action temperature'
 date +%Y,%m,%d-%H:%M:%S
 hist=3
@@ -14,7 +41,7 @@ do
             do
                 for lrt in 7e-4;
                 do
-                    th rlMain.lua -async PPO -network last -seed ${seedSet} -ppo_clip_thr 0.1 -eta ${lrt} -momentum 0.99 -bootstraps 0 -batchSize 32 -hiddenSize 32 -doubleQ false -duel false -recurrent false -optimiser adam -threads ${thrN} -steps $((thrN*120000)) -tau 4 -memSize 18000 -epsilonSteps 90000 -valFreq 1500 -valSteps 2100 -bootstraps 0 -PALpha 0 -entropyBeta 0.001 -lstmHist 10 -rnnHdSizeL1 21 -rnnHdLyCnt 3 -lstmHistUsp 2 -rnnHdSizeL1Usp 21 -rnnHdLyCntUsp 2 -learnStart 2000 -progFreq 1000 -histLen ${hist} -asyncOptimFreq 1 -ciunet rlLoad -uSimScSoft 1 -uppModel rnn_rhn -uppModelUsp cnn_moe -uapFile uap_rhnL3RD3.t7 -uspFile usp_cnnmoe_L2H2K1E30e23.t7 -_id 1foldCI/ci_ppo_lrt_${lrt}_thr_${thrN}_hist${hist}_rwd0_RwdTem${sfxTem}_ActTem${actSfxTem}_ppoEp3_seed${seedSet} -async_valErr_coef 1 -ppo_optim_epo 3 -ciGroup2rwd 0 -ciRwdStMxTemp ${sfxTem} -ciActStMxTemp ${actSfxTem} > /dev/null &
+                    th rlMain.lua -async PPO -network last -seed ${seedSet} -ppo_clip_thr 0.05 -eta ${lrt} -momentum 0.99 -bootstraps 0 -batchSize 32 -hiddenSize 32 -doubleQ false -duel false -recurrent false -optimiser adam -threads ${thrN} -steps $((thrN*120000)) -tau 4 -memSize 18000 -epsilonSteps 90000 -valFreq 1500 -valSteps 2100 -bootstraps 0 -PALpha 0 -entropyBeta 0.001 -lstmHist 10 -rnnHdSizeL1 21 -rnnHdLyCnt 3 -lstmHistUsp 2 -rnnHdSizeL1Usp 21 -rnnHdLyCntUsp 2 -learnStart 2000 -progFreq 1000 -histLen ${hist} -asyncOptimFreq 1 -ciunet rlLoad -uSimScSoft 1 -uppModel rnn_rhn -uppModelUsp cnn_moe -uapFile uap_rhnL3RD3.t7 -uspFile usp_cnnmoe_L2H2K1E30e23.t7 -_id 1foldCI/ci_ppo_lrt_${lrt}_thr_${thrN}_hist${hist}_rwd0_RwdTem${sfxTem}_ActTem${actSfxTem}_ppoEp1_ppoClip0.05_seed${seedSet} -async_valErr_coef 1 -ppo_optim_epo 1 -ciGroup2rwd 0 -ciRwdStMxTemp ${sfxTem} -ciActStMxTemp ${actSfxTem} > /dev/null &
                     wait
                     echo 'done with thrN: ' ${thrN} ', lrt: ' ${lrt} ', RwdsfxTem: ' ${sfxTem} ', ActsfxTem: ' ${actSfxTem}
                 done
