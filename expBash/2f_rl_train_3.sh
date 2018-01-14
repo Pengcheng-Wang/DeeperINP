210b691bb5ef05eb9d472dbd895ee425d2b5b8a7
Pengcheng Wang <pwang8@ncsu.edu>
2018-01-14 18:48:37 -0500

    Added exp

--- expBash/2f_rl_train_3.sh
+++ expBash/2f_rl_train_3.sh
@@ -1,12 +1,39 @@
 #!/usr/bin/env bash
 
+### Continue to train A3C RL models using usp with sampling temperature of -1, 0.67, 0.33
+#echo '2-fold A3C RL training'
+#date +%Y,%m,%d-%H:%M:%S
+#hist=3
+#for sfxTem in -1;
+#do
+#    for seedSet in 2 3;
+#    do
+#        for thrN in 4;
+#        do
+#            for lrt in 7e-4;
+#            do
+#                # train 2-fold of DQN models
+#                th rlMain.lua -async A3C -network last -seed ${seedSet} -ppo_clip_thr 0.1 -eta ${lrt} -momentum 0.99 -bootstraps 0 -batchSize 32 -hiddenSize 32 -doubleQ false -duel false -recurrent false -optimiser adam -threads ${thrN} -steps $((thrN*200000)) -tau 4 -memSize 18000 -epsilonSteps 150000 -valFreq 1500 -valSteps 2100 -bootstraps 0 -PALpha 0 -entropyBeta 0.001 -lstmHist 10 -rnnHdSizeL1 21 -rnnHdLyCnt 3 -lstmHistUsp 2 -rnnHdSizeL1Usp 21 -rnnHdLyCntUsp 2 -learnStart 2000 -progFreq 1000 -histLen ${hist} -asyncOptimFreq 1 -ciunet rlLoad -uSimScSoft 1 -uppModel rnn_rhn -uppModelUsp cnn_moe -uapFile uap_2fold1_rhnHd3RD3St2k.t7 -uspFile usp_2fold1_cnnmoe_L2H2K1E30e23_Ep1k5.t7 -_id 2foldCI/2fold1_ci_a3c_lrt_${lrt}_thr_${thrN}_hist${hist}_rwd0_sfxTem${sfxTem}_seed${seedSet} -async_valErr_coef 1 -ppo_optim_epo 2 -ciGroup2rwd 0 -ciRwdStMxTemp ${sfxTem} > /dev/null &
+#                wait
+#                echo 'done with fold 1, thrN: ' ${thrN} ', lrt: ' ${lrt} ', sfxTem: ' ${sfxTem}
+#                th rlMain.lua -async A3C -network last -seed ${seedSet} -ppo_clip_thr 0.1 -eta ${lrt} -momentum 0.99 -bootstraps 0 -batchSize 32 -hiddenSize 32 -doubleQ false -duel false -recurrent false -optimiser adam -threads ${thrN} -steps $((thrN*200000)) -tau 4 -memSize 18000 -epsilonSteps 150000 -valFreq 1500 -valSteps 2100 -bootstraps 0 -PALpha 0 -entropyBeta 0.001 -lstmHist 10 -rnnHdSizeL1 21 -rnnHdLyCnt 3 -lstmHistUsp 2 -rnnHdSizeL1Usp 21 -rnnHdLyCntUsp 2 -learnStart 2000 -progFreq 1000 -histLen ${hist} -asyncOptimFreq 1 -ciunet rlLoad -uSimScSoft 1 -uppModel rnn_rhn -uppModelUsp cnn_moe -uapFile uap_2fold2_rhnHd3RD3St2k.t7 -uspFile usp_2fold2_cnnmoe_L2H2K1E30e23_Ep1k5.t7 -_id 2foldCI/2fold2_ci_a3c_lrt_${lrt}_thr_${thrN}_hist${hist}_rwd0_sfxTem${sfxTem}_seed${seedSet} -async_valErr_coef 1 -ppo_optim_epo 2 -ciGroup2rwd 0 -ciRwdStMxTemp ${sfxTem} > /dev/null &
+#                wait
+#                echo 'done with fold 2, thrN: ' ${thrN} ', lrt: ' ${lrt} ', sfxTem: ' ${sfxTem}
+#            done
+#        done
+#    done
+#done
+#echo 'Done training'
+#date +%Y,%m,%d-%H:%M:%S
+
+
 ## Continue to train A3C RL models using usp with sampling temperature of -1, 0.67, 0.33
 echo '2-fold A3C RL training'
 date +%Y,%m,%d-%H:%M:%S
 hist=3
 for sfxTem in -1;
 do
-    for seedSet in 2 3;
+    for seedSet in 2;
     do
         for thrN in 4;
         do
