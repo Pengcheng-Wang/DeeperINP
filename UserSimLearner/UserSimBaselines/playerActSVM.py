cda3bf55115a53f9180e619bb92a8bc02ec65082
pwang8 <pwang8@ncsu.edu>
2018-09-07 19:10:18 -0400

    Added svm action pred, with CV.

--- UserSimLearner/UserSimBaselines/playerActSVM.py
+++ UserSimLearner/UserSimBaselines/playerActSVM.py
@@ -5,7 +5,18 @@ Created by pwang8, on Sept 7, 2018.
 import numpy as np
 from pandas import read_csv
 from sklearn.svm import SVC
+from sklearn.model_selection import cross_validate
 
 act_data = read_csv('../../userModelTrained/userStateActOnly.csv')
-print(act_data.shape)
-print(act_data.loc[[0]])
+# shape: (16313, 22)
+act_data_X = act_data.iloc[:, 0:21]
+# shape: (16313, 21)
+act_data_X_norm = (act_data_X - act_data_X.mean()) / (act_data_X.max() - act_data_X.min())
+act_data_Y = act_data.iloc[:, 21:]
+# shape: (16313, 1)
+
+scoring = ['accuracy' ,'f1_macro']
+act_clf = SVC(kernel='linear', C=1)
+scores = cross_validate(act_clf, act_data_X_norm, act_data_Y, scoring=scoring, cv=5, return_train_score=False, verbose=True)
+
+print(scores, file=open('../../userModelTrained/actPredSVM.txt', 'a'))
