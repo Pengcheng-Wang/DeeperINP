b534bd426c7f345bb9951a8092e2f24abe95e73e
pwang8 <pwang8@ncsu.edu>
2018-09-08 15:22:05 -0400

    Added SVM Classifier action predictor, with CV. Use polynomial kernel degree 3, and C=0.1

--- UserSimLearner/UserSimBaselines/playerActSVM.py
+++ UserSimLearner/UserSimBaselines/playerActSVM.py
@@ -15,8 +15,8 @@ act_data_X_norm = (act_data_X - act_data_X.mean()) / (act_data_X.max() - act_dat
 act_data_Y = act_data.iloc[:, 21:]
 # shape: (16313, 1)
 
-scoring = ['accuracy', 'f1_macro', 'f1_micro']
-act_clf = SVC(kernel='rbf', C=0.1)
+scoring = ['accuracy', 'f1_macro', 'f1_micro', 'f1_weighted']
+act_clf = SVC(kernel='poly', C=0.1)
 scores = cross_validate(act_clf, act_data_X_norm, act_data_Y, scoring=scoring, cv=5, return_train_score=False, verbose=True)
 
-print(scores, file=open('../../userModelTrained/actPredSVM_rbf_c.5.txt', 'a'))
+print(scores, file=open('../../userModelTrained/actPredSVM_poly_degree3_c.1.txt', 'a'))
