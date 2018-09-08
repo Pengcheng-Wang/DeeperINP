67e9b681c950131ae227626801409bef199272cc
pwang8 <pwang8@ncsu.edu>
2018-09-08 17:18:37 -0400

    Added Gradient Boosting Classifier action predictor, with CV. Set depth 10, # estimators 20.

--- UserSimLearner/UserSimBaselines/playerActGB.py
+++ UserSimLearner/UserSimBaselines/playerActGB.py
@@ -16,7 +16,7 @@ act_data_Y = act_data.iloc[:, 21:]
 # shape: (16313, 1)
 
 scoring = ['accuracy', 'f1_macro', 'f1_micro', 'f1_weighted']
-act_clf = GradientBoostingClassifier(max_depth=15, n_estimators=30)
+act_clf = GradientBoostingClassifier(max_depth=10, n_estimators=20)
 scores = cross_validate(act_clf, act_data_X_norm, act_data_Y, scoring=scoring, cv=5, return_train_score=False, verbose=True)
 
-print(scores, file=open('../../userModelTrained/actPredGB_dep15_ne30.txt', 'a'))
+print(scores, file=open('../../userModelTrained/actPredGB_dep10_ne20.txt', 'a'))
