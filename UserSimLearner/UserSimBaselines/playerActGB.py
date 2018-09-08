9358ffef706a89890e2942c69bcd63d7938e7fb0
pwang8 <pwang8@ncsu.edu>
2018-09-08 16:58:51 -0400

    Added Gradient Boosting Classifier action predictor, with CV. Use max_depth 15 and n_estimator 30

---
+++ UserSimLearner/UserSimBaselines/playerActGB.py
@@ -0,0 +1,22 @@
+"""
+This script is used to run a Gradient Boosting baseline for player action prediction in CI-RL dataset.
+Created by pwang8, on Sept 8, 2018.
+"""
+import numpy as np
+from pandas import read_csv
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.model_selection import cross_validate
+
+act_data = read_csv('../../userModelTrained/userStateActOnly.csv')
+# shape: (16313, 22)
+act_data_X = act_data.iloc[:, 0:21]
+# shape: (16313, 21)
+act_data_X_norm = (act_data_X - act_data_X.mean()) / (act_data_X.max() - act_data_X.min())
+act_data_Y = act_data.iloc[:, 21:]
+# shape: (16313, 1)
+
+scoring = ['accuracy', 'f1_macro', 'f1_micro', 'f1_weighted']
+act_clf = GradientBoostingClassifier(max_depth=15, n_estimators=30)
+scores = cross_validate(act_clf, act_data_X_norm, act_data_Y, scoring=scoring, cv=5, return_train_score=False, verbose=True)
+
+print(scores, file=open('../../userModelTrained/actPredGB_dep15_ne30.txt', 'a'))
