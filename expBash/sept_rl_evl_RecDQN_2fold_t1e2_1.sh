eb65f09b60ede4b2134b19c6c93a127ebf2e3028
pwang8 <pwang8@ncsu.edu>
2018-09-09 20:48:22 -0400

    Added 2fold cv evaluation for Recurrent DQN.

---
+++ expBash/sept_rl_evl_RecDQN_2fold_t1e2_1.sh
@@ -0,0 +1,148 @@
+#!/usr/bin/env bash
+
+## Sept 9, 2018, by pwang8
+## This is the evaluation of Recurrent DQN RL agents in 2 fold setting, training on 1st, test on 2nd
+ ## Evaluation
+ echo '2fold t1e2 Recurrent DQN RL evaluation'
+ date +%Y,%m,%d-%H:%M:%S
+ hist=3
+ seedSet=2
+ actSfxTem=1
+ ppoEpc=2
+ for thrN in 4;
+ do
+     for lrt in 7e-4;
+     do
+         for sfxTem in -1;
+         do
+             th rlMain.lua -ac_greedy false -async OneStepQ -ciTemCnn 0 -recurrent true -seed ${seedSet} -ppo_clip_thr 0.1 -mode eval -evaTrajs 5000 -ciuTType train -network experiments/2foldCI/2fold1_ci_RecDQN_lrt_${lrt}_thr_${thrN}_hist${hist}_rwd0_RwdTem${sfxTem}_ActTem0.8_seed${seedSet}/164231_0.84015.weights.t7 -eta ${lrt} -momentum 0.99 -bootstraps 0 -batchSize 32 -hiddenSize 32 -doubleQ false -duel false -optimiser adam -threads ${thrN} -steps $((thrN*200000)) -tau 4 -memSize 18000 -epsilonSteps 150000 -valFreq 1500 -valSteps 2100 -bootstraps 0 -PALpha 0 -entropyBeta 0.001 -lstmHist 10 -rnnHdSizeL1 21 -rnnHdLyCnt 3 -lstmHistUsp 2 -rnnHdSizeL1Usp 21 -rnnHdLyCntUsp 2 -learnStart 2000 -progFreq 1000 -histLen ${hist} -asyncOptimFreq 1 -ciunet rlLoad -uSimScSoft 1 -uppModel rnn_rhn -uppModelUsp cnn_moe -uapFile uap_2fold2_rhnHd3RD3St2k.t7 -uspFile usp_2fold2_cnnmoe_L2H2K1E30e23_Ep1k5.t7 -_id 2foldCIEval_Baseline/Train1Evl2/ci_RecDQN_lrt_${lrt}_thr_${thrN}_hist${hist}_rwd0_RwdTem${sfxTem}_ActTem${actSfxTem}_seed${seedSet}/164231_0.84015 -async_valErr_coef 1 -ppo_optim_epo 3 -ciGroup2rwd 0 -ciRwdStMxTemp ${sfxTem} -ciActStMxTemp ${actSfxTem}
+             wait
+             echo 'done with thrN: ' ${thrN} ', lrt: ' ${lrt} ', sfxTem: ' ${sfxTem}
+         done
+     done
+ done
+ wait
+ echo 'Done training'
+ date +%Y,%m,%d-%H:%M:%S
+
+
+ ## Evaluation
+ echo '2fold t1e2 Recurrent DQN RL evaluation'
+ date +%Y,%m,%d-%H:%M:%S
+ hist=3
+ seedSet=2
+ actSfxTem=1
+ ppoEpc=2
+ for thrN in 4;
+ do
+     for lrt in 7e-4;
+     do
+         for sfxTem in -1;
+         do
+             th rlMain.lua -ac_greedy false -async OneStepQ -ciTemCnn 0 -recurrent true -seed ${seedSet} -ppo_clip_thr 0.1 -mode eval -evaTrajs 5000 -ciuTType train -network experiments/2foldCI/2fold1_ci_RecDQN_lrt_${lrt}_thr_${thrN}_hist${hist}_rwd0_RwdTem${sfxTem}_ActTem0.8_seed${seedSet}/331372_0.84528.weights.t7 -eta ${lrt} -momentum 0.99 -bootstraps 0 -batchSize 32 -hiddenSize 32 -doubleQ false -duel false -optimiser adam -threads ${thrN} -steps $((thrN*200000)) -tau 4 -memSize 18000 -epsilonSteps 150000 -valFreq 1500 -valSteps 2100 -bootstraps 0 -PALpha 0 -entropyBeta 0.001 -lstmHist 10 -rnnHdSizeL1 21 -rnnHdLyCnt 3 -lstmHistUsp 2 -rnnHdSizeL1Usp 21 -rnnHdLyCntUsp 2 -learnStart 2000 -progFreq 1000 -histLen ${hist} -asyncOptimFreq 1 -ciunet rlLoad -uSimScSoft 1 -uppModel rnn_rhn -uppModelUsp cnn_moe -uapFile uap_2fold2_rhnHd3RD3St2k.t7 -uspFile usp_2fold2_cnnmoe_L2H2K1E30e23_Ep1k5.t7 -_id 2foldCIEval_Baseline/Train1Evl2/ci_RecDQN_lrt_${lrt}_thr_${thrN}_hist${hist}_rwd0_RwdTem${sfxTem}_ActTem${actSfxTem}_seed${seedSet}/331372_0.84528 -async_valErr_coef 1 -ppo_optim_epo 3 -ciGroup2rwd 0 -ciRwdStMxTemp ${sfxTem} -ciActStMxTemp ${actSfxTem}
+             wait
+             echo 'done with thrN: ' ${thrN} ', lrt: ' ${lrt} ', sfxTem: ' ${sfxTem}
+         done
+     done
+ done
+ wait
+ echo 'Done training'
+ date +%Y,%m,%d-%H:%M:%S
+
+
+ ## Evaluation
+ echo '2fold t1e2 Recurrent DQN RL evaluation'
+ date +%Y,%m,%d-%H:%M:%S
+ hist=3
+ seedSet=2
+ actSfxTem=1
+ ppoEpc=2
+ for thrN in 4;
+ do
+     for lrt in 7e-4;
+     do
+         for sfxTem in -1;
+         do
+             th rlMain.lua -ac_greedy false -async OneStepQ -ciTemCnn 0 -recurrent true -seed ${seedSet} -ppo_clip_thr 0.1 -mode eval -evaTrajs 5000 -ciuTType train -network experiments/2foldCI/2fold1_ci_RecDQN_lrt_${lrt}_thr_${thrN}_hist${hist}_rwd0_RwdTem${sfxTem}_ActTem0.8_seed${seedSet}/598691_0.84644.weights.t7 -eta ${lrt} -momentum 0.99 -bootstraps 0 -batchSize 32 -hiddenSize 32 -doubleQ false -duel false -optimiser adam -threads ${thrN} -steps $((thrN*200000)) -tau 4 -memSize 18000 -epsilonSteps 150000 -valFreq 1500 -valSteps 2100 -bootstraps 0 -PALpha 0 -entropyBeta 0.001 -lstmHist 10 -rnnHdSizeL1 21 -rnnHdLyCnt 3 -lstmHistUsp 2 -rnnHdSizeL1Usp 21 -rnnHdLyCntUsp 2 -learnStart 2000 -progFreq 1000 -histLen ${hist} -asyncOptimFreq 1 -ciunet rlLoad -uSimScSoft 1 -uppModel rnn_rhn -uppModelUsp cnn_moe -uapFile uap_2fold2_rhnHd3RD3St2k.t7 -uspFile usp_2fold2_cnnmoe_L2H2K1E30e23_Ep1k5.t7 -_id 2foldCIEval_Baseline/Train1Evl2/ci_RecDQN_lrt_${lrt}_thr_${thrN}_hist${hist}_rwd0_RwdTem${sfxTem}_ActTem${actSfxTem}_seed${seedSet}/598691_0.84644 -async_valErr_coef 1 -ppo_optim_epo 3 -ciGroup2rwd 0 -ciRwdStMxTemp ${sfxTem} -ciActStMxTemp ${actSfxTem}
+             wait
+             echo 'done with thrN: ' ${thrN} ', lrt: ' ${lrt} ', sfxTem: ' ${sfxTem}
+         done
+     done
+ done
+ wait
+ echo 'Done training'
+ date +%Y,%m,%d-%H:%M:%S
+
+
+# Hi, I'm gonna do t2e1 evaluation for Recurrent DQN, 2fold setting
+
+ ## Evaluation
+ echo '2fold t2e1 Recurrent DQN RL evaluation'
+ date +%Y,%m,%d-%H:%M:%S
+ hist=3
+ seedSet=2
+ actSfxTem=1
+ ppoEpc=2
+ for thrN in 4;
+ do
+     for lrt in 7e-4;
+     do
+         for sfxTem in -1;
+         do
+             th rlMain.lua -ac_greedy false -async OneStepQ -ciTemCnn 0 -recurrent true -seed ${seedSet} -ppo_clip_thr 0.1 -mode eval -evaTrajs 5000 -ciuTType train -network experiments/2foldCI/2fold2_ci_RecDQN_lrt_${lrt}_thr_${thrN}_hist${hist}_rwd0_RwdTem${sfxTem}_ActTem0.8_seed${seedSet}/291617_0.75986.weights.t7 -eta ${lrt} -momentum 0.99 -bootstraps 0 -batchSize 32 -hiddenSize 32 -doubleQ false -duel false -optimiser adam -threads ${thrN} -steps $((thrN*200000)) -tau 4 -memSize 18000 -epsilonSteps 150000 -valFreq 1500 -valSteps 2100 -bootstraps 0 -PALpha 0 -entropyBeta 0.001 -lstmHist 10 -rnnHdSizeL1 21 -rnnHdLyCnt 3 -lstmHistUsp 2 -rnnHdSizeL1Usp 21 -rnnHdLyCntUsp 2 -learnStart 2000 -progFreq 1000 -histLen ${hist} -asyncOptimFreq 1 -ciunet rlLoad -uSimScSoft 1 -uppModel rnn_rhn -uppModelUsp cnn_moe -uapFile uap_2fold1_rhnHd3RD3St2k.t7 -uspFile usp_2fold1_cnnmoe_L2H2K1E30e23_Ep1k5.t7 -_id 2foldCIEval_Baseline/Train2Evl1/ci_RecDQN_lrt_${lrt}_thr_${thrN}_hist${hist}_rwd0_RwdTem${sfxTem}_ActTem${actSfxTem}_seed${seedSet}/291617_0.75986 -async_valErr_coef 1 -ppo_optim_epo 3 -ciGroup2rwd 0 -ciRwdStMxTemp ${sfxTem} -ciActStMxTemp ${actSfxTem}
+             wait
+             echo 'done with thrN: ' ${thrN} ', lrt: ' ${lrt} ', sfxTem: ' ${sfxTem}
+         done
+     done
+ done
+ wait
+ echo 'Done training'
+ date +%Y,%m,%d-%H:%M:%S
+
+
+ ## Evaluation
+ echo '2fold t2e1 Recurrent DQN RL evaluation'
+ date +%Y,%m,%d-%H:%M:%S
+ hist=3
+ seedSet=2
+ actSfxTem=1
+ ppoEpc=2
+ for thrN in 4;
+ do
+     for lrt in 7e-4;
+     do
+         for sfxTem in -1;
+         do
+             th rlMain.lua -ac_greedy false -async OneStepQ -ciTemCnn 0 -recurrent true -seed ${seedSet} -ppo_clip_thr 0.1 -mode eval -evaTrajs 5000 -ciuTType train -network experiments/2foldCI/2fold2_ci_RecDQN_lrt_${lrt}_thr_${thrN}_hist${hist}_rwd0_RwdTem${sfxTem}_ActTem0.8_seed${seedSet}/637354_0.74265.weights.t7 -eta ${lrt} -momentum 0.99 -bootstraps 0 -batchSize 32 -hiddenSize 32 -doubleQ false -duel false -optimiser adam -threads ${thrN} -steps $((thrN*200000)) -tau 4 -memSize 18000 -epsilonSteps 150000 -valFreq 1500 -valSteps 2100 -bootstraps 0 -PALpha 0 -entropyBeta 0.001 -lstmHist 10 -rnnHdSizeL1 21 -rnnHdLyCnt 3 -lstmHistUsp 2 -rnnHdSizeL1Usp 21 -rnnHdLyCntUsp 2 -learnStart 2000 -progFreq 1000 -histLen ${hist} -asyncOptimFreq 1 -ciunet rlLoad -uSimScSoft 1 -uppModel rnn_rhn -uppModelUsp cnn_moe -uapFile uap_2fold1_rhnHd3RD3St2k.t7 -uspFile usp_2fold1_cnnmoe_L2H2K1E30e23_Ep1k5.t7 -_id 2foldCIEval_Baseline/Train2Evl1/ci_RecDQN_lrt_${lrt}_thr_${thrN}_hist${hist}_rwd0_RwdTem${sfxTem}_ActTem${actSfxTem}_seed${seedSet}/637354_0.74265 -async_valErr_coef 1 -ppo_optim_epo 3 -ciGroup2rwd 0 -ciRwdStMxTemp ${sfxTem} -ciActStMxTemp ${actSfxTem}
+             wait
+             echo 'done with thrN: ' ${thrN} ', lrt: ' ${lrt} ', sfxTem: ' ${sfxTem}
+         done
+     done
+ done
+ wait
+ echo 'Done training'
+ date +%Y,%m,%d-%H:%M:%S
+
+
+ ## Evaluation
+ echo '2fold t2e1 Recurrent DQN RL evaluation'
+ date +%Y,%m,%d-%H:%M:%S
+ hist=3
+ seedSet=2
+ actSfxTem=1
+ ppoEpc=2
+ for thrN in 4;
+ do
+     for lrt in 7e-4;
+     do
+         for sfxTem in -1;
+         do
+             th rlMain.lua -ac_greedy false -async OneStepQ -ciTemCnn 0 -recurrent true -seed ${seedSet} -ppo_clip_thr 0.1 -mode eval -evaTrajs 5000 -ciuTType train -network experiments/2foldCI/2fold2_ci_RecDQN_lrt_${lrt}_thr_${thrN}_hist${hist}_rwd0_RwdTem${sfxTem}_ActTem0.8_seed${seedSet}/703313_0.74913.weights.t7 -eta ${lrt} -momentum 0.99 -bootstraps 0 -batchSize 32 -hiddenSize 32 -doubleQ false -duel false -optimiser adam -threads ${thrN} -steps $((thrN*200000)) -tau 4 -memSize 18000 -epsilonSteps 150000 -valFreq 1500 -valSteps 2100 -bootstraps 0 -PALpha 0 -entropyBeta 0.001 -lstmHist 10 -rnnHdSizeL1 21 -rnnHdLyCnt 3 -lstmHistUsp 2 -rnnHdSizeL1Usp 21 -rnnHdLyCntUsp 2 -learnStart 2000 -progFreq 1000 -histLen ${hist} -asyncOptimFreq 1 -ciunet rlLoad -uSimScSoft 1 -uppModel rnn_rhn -uppModelUsp cnn_moe -uapFile uap_2fold1_rhnHd3RD3St2k.t7 -uspFile usp_2fold1_cnnmoe_L2H2K1E30e23_Ep1k5.t7 -_id 2foldCIEval_Baseline/Train2Evl1/ci_RecDQN_lrt_${lrt}_thr_${thrN}_hist${hist}_rwd0_RwdTem${sfxTem}_ActTem${actSfxTem}_seed${seedSet}/703313_0.74913 -async_valErr_coef 1 -ppo_optim_epo 3 -ciGroup2rwd 0 -ciRwdStMxTemp ${sfxTem} -ciActStMxTemp ${actSfxTem}
+             wait
+             echo 'done with thrN: ' ${thrN} ', lrt: ' ${lrt} ', sfxTem: ' ${sfxTem}
+         done
+     done
+ done
+ wait
+ echo 'Done training'
+ date +%Y,%m,%d-%H:%M:%S
